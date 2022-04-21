5450280a4c36e91722c68f7d54734c7207761f3c
tiagokp97 <tiagopesch@gmail.com>
2022-04-21 13:32:49 -0300

    build: more global styles and icons  added

--- src/styles/global.js
+++ src/styles/global.js
@@ -6,8 +6,6 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     outline: 0;
-
-
 }
 
 :root {
@@ -20,6 +18,24 @@ export default createGlobalStyle`
     --grey-20: #343B41;
     --grey-30: #212529;
     --grey-40: #121214;
-
 }
+
+body {
+    background: #121214;
+    color: black;
+}
+
+body, input, button {
+    font-family: "inter";
+    font-size: 1rem;
+}
+
+button {
+    cursor: pointer;
+}
+
+a {
+    text-decoration: none;
+}
+
 `;
