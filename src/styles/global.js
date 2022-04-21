383435b85625fc19943b89b3e0762374289702e4
tiagokp97 <tiagopesch@gmail.com>
2022-04-21 13:20:23 -0300

    build: create react-app, add dependencies and imported fonts and colors

---
+++ src/styles/global.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+
+
+}
+
+:root {
+    --pink: #FF577F;
+    --pink-focus: #FF427F;
+    --disable: #59323F;
+    --sucess:#3FE864;
+    --failed: #E83F5B;
+    --grey-10: #868E96;
+    --grey-20: #343B41;
+    --grey-30: #212529;
+    --grey-40: #121214;
+
+}
+`;
